955110e4df8367eb06ab4b622bc4101bb4d911be
Asiel <60462819+Asiell@users.noreply.github.com>
2020-06-30 02:04:51 +1000

    404 The requested path could not be found fix #4

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/App.js',
     output: {
         filename: 'bundle.js',
-        path: path.join(__dirname, 'public')
+        path: path.join(__dirname, '/public')
     },
     module: {
         rules: [
