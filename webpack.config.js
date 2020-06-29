990665b558794727cf626c75b2bde2f69cd8f57e
Asiel <60462819+Asiell@users.noreply.github.com>
2020-06-30 00:59:54 +1000

    Updated webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/App.js',
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'public')
@@ -22,7 +22,7 @@ module.exports = {
                     loader: 'file-loader',
                   },
                 ],
-              }
+            }
         ]
     }
 };
