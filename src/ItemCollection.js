89d0346b7257df601b0185192f2eebd249ec26df
Asiel <60462819+Asiell@users.noreply.github.com>
2020-07-06 02:52:36 +1000

    added list view for item collection

--- src/ItemCollection.js
+++ src/ItemCollection.js
@@ -10,6 +10,7 @@ export default function HomePage(){
     const [listItems, setListItems] = useState([]);
     const [searchedItem, setSearchedItem] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+    const [listView, setListView] = useState(false);
 
     // Equivalent of componentDidMount
     useEffect(()=>{
@@ -83,78 +84,147 @@ export default function HomePage(){
             <div class="loader"></div> 
             :
             <div class="pt-32 max-w-full w-screen lg:max-w-full">
-                <h2 class="text-4xl ml-32 inline"> Item Collection </h2>
-                <div class="pb-4 inline-block">
-                    <input 
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-900 leading-tight focus:outline-none focus:shadow-outline" 
-                        id="item-collection-search" 
-                        type="text" 
-                        placeholder="Search for an item" 
-                        onChange = {e => setSearchTerm(e.target.value)}
-                        onKeyPress= {e => handleKeyPress(e)}
-                    />
+                <div class="flex flex-wrap pb-8">
+                    <div class="w-3/5">
+                    <h2 class="text-4xl ml-32 inline"> Item Collection </h2>
+                    <div class="inline-block my-auto ml-3">
+                        <input 
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-900 leading-tight focus:outline-none focus:shadow-outline" 
+                            id="item-collection-search" 
+                            type="text" 
+                            placeholder="Search for an item" 
+                            onChange = {e => setSearchTerm(e.target.value)}
+                            onKeyPress= {e => handleKeyPress(e)}
+                        />
+                    </div>
+                    </div>
+                    {/* Toggle button */}
+                    <label 
+                        for="toogleA"
+                        class="content-center cursor-pointer ml-3 text-sm my-auto justify-end flex w-1/5 font-lato"
+                    >
+                        {/* toggle */}
+                        <div class="relative inline-block">
+                        {/* input */}
+                        <input id="toogleA" type="checkbox" class="hidden" 
+                        onClick={() => setListView(!listView)} 
+                        />
+                        {/* line */}
+                        <div
+                            class="toggle__line w-10 h-4 bg-grey-400 rounded-full shadow-inner"
+                        ></div>
+                        {/* dot */}
+                        <div
+                            class="toggle__dot absolute w-6 h-6 bg-white rounded-full shadow inset-y-0 left-0"
+                        ></div>
+                        </div>
+                        {/* Text */}
+                        <div
+                        class="ml-3 text-gray-700 inline-block"
+                        >
+                        List View
+                        </div>
+                    </label>
                 </div>
-            <div class="justify-center flex-wrap lg:flex pl-10 ">
+            <div class="justify-center flex-wrap lg:flex pl-16 pr-5">
+
+                { listView ? 
+                    <table class="table-auto w-full pb-32">
+                        <thead>
+                            <tr>
+                            <th class="text-left border-b-4 border-gray-900 px-4 py-2 bg-gray-500 w-1/4">Item Name</th>
+                            <th class="border-b-4 border-gray-900 px-4 py-2 bg-gray-500 w-1/4">Variant</th>
+                            <th class="border-b-4 border-gray-900 px-4 py-2 bg-gray-500 w-1/4">Pattern</th>
+                            <th class="border-b-4 border-gray-900 px-4 py-2 bg-gray-500 w-1/4">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        
+                        {listItems.map((item)=>{
+                            return(
+                                <tr>
+                                {/* <td class="border-b border-gray-900 px-4 py-2 bg-gray-300">
+                                    <img class="mx-auto img-icon object-cover object-center" src={item.image_uri} alt="avatar" />
+                                </td> */}
+                                <td class="border-b border-gray-900 px-4 py-2 bg-gray-300">
+                                    <h1 class="text-xl font-semibold text-gray-800">{item.name['name-USen']}</h1>
+                                </td>
+                                <td class="text-center border-b border-gray-900 px-4 py-2 bg-gray-300">{item.variant !== null ? item.variant : "☓"}</td>
+                                <td class="text-center border-b border-gray-900 px-4 py-2 bg-gray-300">{item.pattern !== null ? item.pattern : "☓"}</td>
+                                <td class=" text-right border-b border-gray-900 px-4 py-2 bg-gray-300"><span class="text-2xl"><i class="fas fa-user-friends"></i><input class="ml-2" type="checkbox" /></span></td>
+                                </tr>
+                            )
+                        })}
+                            
+                        </tbody>
+                    </table>
+                :
+
+                    searchTerm !== "" ? 
+                    searchedItem.map((item) => {
+                        return(
+                            <div class="px-10 pb-5" key={item.name['name-USen'] + " " + item.variant + " " + item.pattern} onClick={() => toggleHidden(item.name['name-USen'] + " " + item.variant + " " + item.pattern)} >
+                            <div class="relative max-w-sm bg-white shadow-lg rounded-lg overflow-hidden my-4">
+                                
+                                <div class="absolute collected hidden" id={item.name['name-USen'] + " " + item.variant + " " + item.pattern}>
+                                    <img src={stamp} />
+                                </div>
+                                <div class="" id={item.name['name-USen'] + " " + item.variant + " " + item.pattern + " img"}>
+                                <img class="mx-auto object-cover object-center" src={item.image_uri} alt="avatar" />
+                                
+                                <div class="py-4 px-6">
+                                    <h1 class="text-2xl font-semibold text-gray-800">{item.name['name-USen']}</h1>
+                                    <div class="flex items-center mt-4 text-gray-700">
+                                        { item.variant !== null ? <span class="flex"><i class="fas fa-palette"></i>
+                                        <h1 class="px-2 text-sm">{item.variant}</h1></span> : null}
+                                        { item.pattern !== null ? <span class="flex"><i class="fas fa-tint"></i>
+                                        <h1 class="px-2 text-sm">{item.pattern}</h1></span> : null }
+                                    </div>
+                                    
+                                </div>
+                                </div>
+                            </div>
+                            <div class="px-4 flex items-center text-gray-700">
+                                <i class="fas fa-user-friends"></i>
+                                <h1 class="px-2 text-sm">Your friend has this!</h1>
+                            </div>
+                            </div>
+                        )
+                    })
+                    : listItems.map((item)=>{
+                        return (
+                            <div class="px-10 pb-5" key={item.name['name-USen'] + " " + item.variant + " " + item.pattern} onClick={() => toggleHidden(item.name['name-USen'] + " " + item.variant + " " + item.pattern)} >
+                            <div class="relative max-w-sm bg-white shadow-lg rounded-lg overflow-hidden my-4">
+                                
+                                <div class="absolute collected hidden" id={item.name['name-USen'] + " " + item.variant + " " + item.pattern}>
+                                    <img src={stamp} />
+                                </div>
+                                <div class="" id={item.name['name-USen'] + " " + item.variant + " " + item.pattern + " img"}>
+                                <img class="mx-auto object-cover object-center" src={item.image_uri} alt="avatar" />
+                                
+                                <div class="py-4 px-6">
+                                    <h1 class="text-2xl font-semibold text-gray-800">{item.name['name-USen']}</h1>
+                                    <div class="flex items-center mt-4 text-gray-700">
+                                        { item.variant !== null ? <span class="flex"><i class="fas fa-palette"></i>
+                                        <h1 class="px-2 text-sm">{item.variant}</h1></span> : null}
+                                        { item.pattern !== null ? <span class="flex"><i class="fas fa-tint"></i>
+                                        <h1 class="px-2 text-sm">{item.pattern}</h1></span> : null }
+                                    </div>
+                                    
+                                </div>
+                                </div>
+                            </div>
+                            <div class="px-4 flex items-center text-gray-700">
+                                <i class="fas fa-user-friends"></i>
+                                <h1 class="px-2 text-sm">Your friend has this!</h1>
+                            </div>
+                            </div>)
+                    })
+                }
+                    
             
-            {
-                searchTerm !== "" ? 
-                searchedItem.map((item) => {
-                    return(
-                        <div class="px-10 pb-5" key={item.name['name-USen'] + " " + item.variant + " " + item.pattern} onClick={() => toggleHidden(item.name['name-USen'] + " " + item.variant + " " + item.pattern)} >
-                        <div class="relative max-w-sm bg-white shadow-lg rounded-lg overflow-hidden my-4">
-                            
-                            <div class="absolute collected hidden" id={item.name['name-USen'] + " " + item.variant + " " + item.pattern}>
-                                {/* <i class="far fa-check-circle"></i> */}
-                                <img src={stamp} />
-                            </div>
-                            <div class="" id={item.name['name-USen'] + " " + item.variant + " " + item.pattern + " img"}>
-                            <img class="mx-auto object-cover object-center" src={item.image_uri} alt="avatar" />
-                            
-                            <div class="py-4 px-6">
-                                <h1 class="text-2xl font-semibold text-gray-800">{item.name['name-USen']}</h1>
-                                <div class="flex items-center mt-4 text-gray-700">
-                                    <i class="fas fa-palette"></i>
-                                    <h1 class="px-2 text-sm">{item.variant}</h1>
-                                </div>
-                                
-                            </div>
-                            </div>
-                        </div>
-                        <div class="px-4 flex items-center text-gray-700">
-                            <i class="fas fa-user-friends"></i>
-                            <h1 class="px-2 text-sm">Your friend has this!</h1>
-                        </div>
-                        </div>
-                    )
-                })
-                : listItems.map((item)=>{
-                    return (
-                        <div class="px-10 pb-5" key={item.name['name-USen'] + " " + item.variant + " " + item.pattern} onClick={() => toggleHidden(item.name['name-USen'] + " " + item.variant + " " + item.pattern)} >
-                        <div class="relative max-w-sm bg-white shadow-lg rounded-lg overflow-hidden my-4">
-                            
-                            <div class="absolute collected hidden" id={item.name['name-USen'] + " " + item.variant + " " + item.pattern}>
-                                {/* <i class="far fa-check-circle"></i> */}
-                                <img src={stamp} />
-                            </div>
-                            <div class="" id={item.name['name-USen'] + " " + item.variant + " " + item.pattern + " img"}>
-                            <img class="mx-auto object-cover object-center" src={item.image_uri} alt="avatar" />
-                            
-                            <div class="py-4 px-6">
-                                <h1 class="text-2xl font-semibold text-gray-800">{item.name['name-USen']}</h1>
-                                <div class="flex items-center mt-4 text-gray-700">
-                                    <i class="fas fa-palette"></i>
-                                    <h1 class="px-2 text-sm">{item.variant}</h1>
-                                </div>
-                                
-                            </div>
-                            </div>
-                        </div>
-                        <div class="px-4 flex items-center text-gray-700">
-                            <i class="fas fa-user-friends"></i>
-                            <h1 class="px-2 text-sm">Your friend has this!</h1>
-                        </div>
-                        </div>)
-            })}
+            
+                
             {/* <div class="px-10 pb-5">
             <div class="max-w-sm bg-white shadow-lg rounded-lg overflow-hidden my-4">
                 <img class="w-full h-full object-cover object-center" src="https://acnhcdn.com/latest/FtrIcon/FtrAcorsticguitar_Remake_0_0.png" alt="avatar" />
